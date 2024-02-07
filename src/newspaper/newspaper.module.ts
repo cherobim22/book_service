b6a1bdb94d6979502ab6b9d80d1960802cc3d14d
Lucas Cherobbim <lucascherobim@hotmail.com>
2024-02-07 17:09:24 -0300

    feat: add redis cache

--- src/newspaper/newspaper.module.ts
+++ src/newspaper/newspaper.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { NewspaperService } from './newspaper.service';
 import { NewspaperController } from './newspaper.controller';
 import { CacheModule } from '@nestjs/cache-manager';
+import type { ClientOpts } from 'redis';
+
 
 @Module({
-  imports:[CacheModule.register()],
+  imports:[CacheModule.register<ClientOpts>({ 
+
+    host: 'localhost',
+    port: 6379 })],
   providers: [NewspaperService],
   controllers: [NewspaperController]
 })
